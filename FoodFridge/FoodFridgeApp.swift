61ee38a35db2e5b31c818b26ed0e6a1880d0b9fa
jessie <jessie.pastan@gmail.com>
2023-12-20 22:31:00 -0500

    Initial Commit

---
+++ FoodFridge/FoodFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFridgeApp.swift
+//  FoodFridge
+//
+//  Created by Jessie Pastan on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
