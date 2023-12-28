e8d5c25dfadd1ef50dea4c93d48d14d461ceee31
jessie <jessie.pastan@gmail.com>
2023-12-27 23:34:06 -0500

    implement tag items

---
+++ FoodFridge/Core/SelectionIngredientView/TagsView.swift
@@ -0,0 +1,18 @@
+//
+//  TagsView.swift
+//  FoodFridge
+//
+//  Created by Jessie Pastan on 12/27/23.
+//
+
+import SwiftUI
+
+struct TagsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TagsView()
+}
