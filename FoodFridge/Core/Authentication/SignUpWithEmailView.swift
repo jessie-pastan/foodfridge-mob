00410f67b84b62f2b75d9a1cca8cd34a9bae7a61
Wararat <princess_princeshin@hotmail.com>
2023-12-28 12:19:32 -0500

    my first try to create the page. I think it's too many lines

--- FoodFridge/Core/Authentication/SignUpWithEmailView.swift
+++ FoodFridge/Core/Authentication/SignUpWithEmailView.swift
@@ -9,10 +9,33 @@ import SwiftUI
 
 struct SignUpWithEmailView: View {
     var body: some View {
-        Text("Sign up with Email")
+        Text("Create account")
+            .font(.custom("CourierPrime-Bold", size: 30))
+            .padding(.leading, -80.0)
+            .frame(height: 3.0)
+            .offset(y: -300)
+            .foregroundColor(Color.black)
+        VStack{
+            Text("Username")
+                .font(.custom("CourierPrime-Regular", size: 20))
+                .padding(.leading, -150.0)
+                .frame(width: 2.0, height: 2.0)
+                .offset(y: -200)
+                .foregroundColor(Color.black)
+            Text("Password")
+                .font(.custom("CourierPrime-Regular", size: 20))
+                .padding(.leading, -150.0)
+                .frame(width: 2.0, height: 2.0)
+                .offset(y: -120)
+                .foregroundColor(Color.black)
+        }
     }
+    
 }
-
-#Preview {
+        
+        
+        
+ #Preview {
     SignUpWithEmailView()
-}
+    
+        }
---
+++ FoodFridge/Core/UserProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  FoodFridge
+//
+//  Created by keaw yuntreesing on 12/27/23.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        VStack {
+            Text("Hello")
+                
+        }
+    }
+}
+#Preview {
+    UserProfileView()
+}
+
