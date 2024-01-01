ecbd4039f9a5636b2c833d51c1fff7231f327ff2
Wararat <princess_princeshin@hotmail.com>
2024-01-01 17:23:32 -0500

    update line

--- FoodFridge/Core/Authentication/SignUpWithEmailView.swift
+++ FoodFridge/Core/Authentication/SignUpWithEmailView.swift
@@ -8,34 +8,46 @@
 import SwiftUI
 
 struct SignUpWithEmailView: View {
+    @State var name: String = ""
+    
     var body: some View {
         Text("Create account")
-            .font(.custom("CourierPrime-Bold", size: 30))
-            .padding(.leading, -80.0)
-            .frame(height: 3.0)
-            .offset(y: -300)
-            .foregroundColor(Color.black)
+              .font(.custom("CourierPrime-Bold", size: 40))
+              .padding(.top, -105)
+        
         VStack{
-            Text("Username")
-                .font(.custom("CourierPrime-Regular", size: 20))
-                .padding(.leading, -150.0)
-                .frame(width: 2.0, height: 2.0)
-                .offset(y: -200)
+            TextField("username", text: $name)
+                .background(Color.button4)
                 .foregroundColor(Color.black)
-            Text("Password")
-                .font(.custom("CourierPrime-Regular", size: 20))
-                .padding(.leading, -150.0)
-                .frame(width: 2.0, height: 2.0)
-                .offset(y: -120)
+                .font(.custom("CourierPrime-Regular", size: 30))
+                .cornerRadius(70)
+                .padding()
+            TextField("password", text: $name)
+                .background(Color.button4)
                 .foregroundColor(Color.black)
+                .font(.custom("CourierPrime-Regular", size: 30))
+                .cornerRadius(70)
+                .padding()
+            Button(action: {}) {
+                Spacer()
+                Text("Sign Up")
+                    .font(.title3)
+                    .frame(maxWidth: 300)
+                    .background(Color.button2)
+                    .foregroundColor(Color.black)
+                    .padding(.bottom)
+                    .cornerRadius(140)
+                Spacer()
+            }
+        }
+        HStack{
+            Spacer()
+            Text("By using the app, you agree to our Terms of use and Privacy policy")
+                .padding(.bottom)
         }
     }
-    
 }
-        
-        
-        
- #Preview {
-    SignUpWithEmailView()
-    
+  #Preview
+        {
+            SignUpWithEmailView()
         }
